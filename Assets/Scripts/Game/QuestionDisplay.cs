95f0c0cbc923110672bd26d7985266f874832ffc
Nic1493 <nic.choy107@gmail.com>
2020-06-22 16:16:33 -0400

    Game updates
    question display textbox now scrolls up when the player submits an answer, instead of just trimming the topmost line of the textbox

--- Assets/Scripts/Game/QuestionDisplay.cs
+++ Assets/Scripts/Game/QuestionDisplay.cs
@@ -1,22 +1,43 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using TMPro;
 
 public class QuestionDisplay : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI questionDisplayBox;
-    [SerializeField] TextMeshProUGUI answerDisplayBox;
+    [SerializeField] RectTransform questionDisplayBox;
+    [SerializeField] RectTransform answerDisplayBox;
+    TextMeshProUGUI answerDisplay;
 
     void Awake()
     {
         FindObjectOfType<Keyboard>().SubmitAnswerAction += OnSubmitAnswer;
+
+        answerDisplay = answerDisplayBox.GetComponent<TextMeshProUGUI>();
     }
 
     void OnSubmitAnswer(int answer)
     {
-        //trim first line of questionDisplayBox
-        int newLineIndex = questionDisplayBox.text.IndexOf('\n');
-        string s = questionDisplayBox.text.Substring(newLineIndex + 1);
+        answerDisplay.text += answer.ToString() + '\n';
+        StartCoroutine(Scroll(questionDisplayBox));
+        StartCoroutine(Scroll(answerDisplayBox));
+    }
 
-        questionDisplayBox.text = s;
+    //☢ WARNING ☢ MAGIC NUMBERS BELOW
+    IEnumerator Scroll(RectTransform rt)
+    {
+        float startPos = rt.anchoredPosition.y;
+        float endPos = rt.anchoredPosition.y + 172;
+
+        float totalLerpTime = 0.1f;
+        float currentLerpTime = 0;
+
+        while (currentLerpTime < totalLerpTime)
+        {
+            yield return new WaitForEndOfFrame();
+            currentLerpTime += Time.deltaTime;
+
+            float yPos = Mathf.Lerp(startPos, endPos, currentLerpTime / totalLerpTime);
+            rt.anchoredPosition = new Vector2(rt.anchoredPosition.x, yPos);
+        }
     }
 }
