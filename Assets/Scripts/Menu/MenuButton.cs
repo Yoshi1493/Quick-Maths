0a9ce23e534012210c131d84cb2a8019882638d6
Nic1493 <nic.choy107@gmail.com>
2020-06-22 15:05:04 -0400

    UI updates
    created MenuButton script to expand my button animation options
    set menu button text to be underlined instead of changing font size when pressed

---
+++ Assets/Scripts/Menu/MenuButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class MenuButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    TextMeshProUGUI buttonText;
+
+    void Awake()
+    {
+        buttonText = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        buttonText.fontStyle |= FontStyles.Underline;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        buttonText.fontStyle ^= FontStyles.Underline;
+    }
+}
